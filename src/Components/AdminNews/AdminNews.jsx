51091969608e29e574a06d989af37beb33cdb283
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2023-11-07 14:07:48 +0200

    fixed style

--- src/Components/AdminNews/AdminNews.jsx
+++ src/Components/AdminNews/AdminNews.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 
-import PlusSvg from './PlusSvg/PlusSvg';
 import PopUpEdit from './PopUpEdit/PopUpEdit';
 import PopUpAdd from './PopUpAdd/PopUpAdd';
 import CrossSvg from './CrossSvg/CrossSvg';
@@ -12,7 +11,9 @@ import fetchData from '../../const/fetchData';
 import deleteDataItem from '../../const/deleteDataItem';
 import editDataItem from '../../const/editDataItem';
 import changePosition from '../../const/changePosition';
+import checkLength from '../../const/checkLength';
 import updateList from '../../const/updateList';
+import heart from '/img/heart.svg'
 
 import s from './AdminNews.module.scss';
 
@@ -26,23 +27,54 @@ export default function AdminNews() {
     const fetch = () => fetchData(dataRef, setItems, true);
     const update = () => updateList(dataRef, fetch);
 
-    const renderItems = (classWrapper, start, end) => {
+    const titles = {
+        first: {
+            value: 'Головна Новина',
+            style: s.first
+        },
+        five: {
+            value: 'Наступні 5',
+            style: s.five
+        },
+        other: {
+            value: 'Інші Новини',
+            style: s.other
+        }
+    }
+
+    const renderItems = (wrapper, start, end) => {
         return (
-            <div className={classWrapper}>
-                {items.slice(start, end).map((item) => (
-                    <div className={s.item} key={item.key}>
-                        <div className={s.item_content}>
-                            <div className={s.item__img} style={{ backgroundImage: `url(${item.image})` }}></div>
-                            <div className={s.item__name}>{item.title}</div>
+            <div className={s.wrapperBlock + ' ' + wrapper.style}>
+                <div className={s.title}>
+                    {wrapper.value}
+                </div>
+                <div className={s.items}>
+                    {items.slice(start, end).map((item) => (
+                        <div className={s.item} key={item.key}>
+                            <div className={s.item_content}>
+                                <div className={s.date}> {item.date} </div>
+                                <div className={s.name}> {checkLength(item.title, 30)} </div>
+                                <div className={s.like}>
+                                    <img src={heart} alt="heart" />
+                                    <div className={s.count}>{item.likes}</div>
+                                </div>
+                            </div>
+                            <div className={s.item_setting}>
+                                <div className={s.arrows}>
+                                    <div className={s.arrow + ' ' + s.arrowBottom} onClick={() => changePosition(item, items, dataRef, fetch, true)}><Arrow /></div>
+                                    <div className={s.arrow + ' ' + s.arrowTop} onClick={() => changePosition(item, items, dataRef, fetch, false)}><Arrow /></div>
+                                </div>
+                                <div className={s.setting} onClick={() => editDataItem(item, setDataItem, setPopUpEdit)}>
+                                    <SettingSvg />
+                                </div>
+                                <div className={s.cross} onClick={() => deleteDataItem(item.key, dataRef, setItems)}>
+                                    <CrossSvg />
+                                </div>
+                                <div className={s.setting__cross} onClick={() => deleteDataItem(item.key, dataRef, setItems)}><CrossSvg /></div>
+                            </div>
                         </div>
-                        <div className={s.item_setting}>
-                            <div className={s.setting__arrow + ' ' + s.arrowBottom} onClick={() => changePosition(item, items, dataRef, fetch, true)}><Arrow /></div>
-                            <div className={s.setting__arrow + ' ' + s.arrowTop} onClick={() => changePosition(item, items, dataRef, fetch, false)}><Arrow /></div>
-                            <div className={s.setting__edit} onClick={() => editDataItem(item, setDataItem, setPopUpEdit)}><SettingSvg /></div>
-                            <div className={s.setting__cross} onClick={() => deleteDataItem(item.key, dataRef, setItems)}><CrossSvg /></div>
-                        </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         )
     }
@@ -53,11 +85,13 @@ export default function AdminNews() {
 
     return (
         <section>
-            <div className={s.added} onClick={() => setPopUpAdd(true)}><PlusSvg /></div>
+            <div className={s.added} onClick={() => setPopUpAdd(true)}>
+                Додати Новину
+            </div>
             <div className={s.news}>
-                {renderItems(s.item_wrapper + ' ' + s.first, 0, 1,)}
-                {renderItems(s.item_wrapper + ' ' + s.second, 1, 6)}
-                {renderItems(s.item_wrapper + ' ' + s.third, 6)}
+                {renderItems(titles.first, 0, 1,)}
+                {renderItems(titles.five, 1, 6)}
+                {renderItems(titles.other, 6)}
             </div>
             {popUpAdded && <PopUpAdd dataRef={dataRef} fetchData={fetch} setPopUpAdd={setPopUpAdd} changePosition={changePosition} updateList={update} />}
             {popUpEdit && <PopUpEdit dataRef={dataRef} fetchData={fetch} item={dataItem} setPopUpEdit={setPopUpEdit} />}
